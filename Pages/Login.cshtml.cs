55b64cb7972ab09eca81f4234b767a3e85c2e972
David Gallo <david.gallo747@gmail.com>
2021-03-27 14:28:18 -0400

    login part 2

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -14,21 +14,8 @@ namespace Adm4379Example.Pages
     public class LogInModel : PageModel
     {
         private readonly ILogger<LogInModel> _logger;
-        //deprecated, for reading from JSON file -----------------------------------------------
-        //private JsonCasesReaderService service; 
-        //public IEnumerable<Cases> Cases;
-
-        //public LogInModel(ILogger<LogInModel> logger, JsonCasesReaderService jcasesrs)
-        //{
-        //    _logger = logger;
-        //    service = jcasesrs;
-        //}
-
-        public CountriesService MyCountriesService;
-        public IEnumerable<Countries> Countries;
-
-        public CasesService MyCasesService;
-        public IEnumerable<Cases> Cases;
+        public UsersService MyUsersService;
+        public IEnumerable<Users> Users;
 
         public LogInModel(ILogger<LogInModel> logger, UsersService usersServ)
         {
@@ -36,7 +23,7 @@ namespace Adm4379Example.Pages
             MyUsersService = usersServ;
         }
 
-        public bool ValidateLogin(string email, string password)
+        public IActionResult ValidateLogin(string email, string password)
         {
             Users = MyUsersService.GetUsers();
             print(Users)
@@ -44,9 +31,9 @@ namespace Adm4379Example.Pages
             for user in Users {
                 if email == Users.email && password = Users.password {
                     HttpContext.Session.SetString("username", email);
-                    return true
+                    return RedirectToPage("Dashboard");
                 } else {
-                    return false
+                    return Page();
                 }
             }
         }
